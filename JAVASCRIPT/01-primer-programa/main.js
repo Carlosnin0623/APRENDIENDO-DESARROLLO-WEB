84176027df62439444e62e855ca67191e45d1e3d
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-04-29 16:58:07 -0400

    Variables

---
+++ JAVASCRIPT/01-primer-programa/main.js
@@ -0,0 +1,11 @@
+
+/* Alertas en javascript   */
+
+// alert("Bienvenido al master en Javascript");
+
+
+/* Console.log() Permite mostrar informacion por la consola del navegador */
+
+console.log("Voy a ser desarollador FullStack");
+
+console.log(124 + 11);
---
+++ JAVASCRIPT/02-Variables/VariableLet.js
@@ -0,0 +1,16 @@
+/*
+ Variables
+Es una caja donde guardo una información
+
+*/
+
+let heroe = "Spiderman";
+let universo = "Marvel";
+let edad = 25;
+
+let heroe_y_universo = heroe +" es un superheroe del universo "+ universo +" y tiene "+edad + " años de edad";
+
+
+console.log(heroe);
+alert(heroe_y_universo);
+
