858479712df660f64435ee034c00e5ffd2064d68
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-04-30 16:43:37 -0400

    Operadores en Javascript

--- JAVASCRIPT/05-Operadores/Operadores.js
+++ JAVASCRIPT/05-Operadores/Operadores.js
@@ -18,12 +18,91 @@ console.log("El resto es:", resto);
 console.log("La potencia de " + numero1 + " es " + potencia);
 
 // Asignacion
+let numerito = 17;
+
+// Adicion
+numerito += 3;
+
+numerito *= 2;
+
+console.log(numerito);
 
 // Comparacion
 
+let numerazo = 20;
+
+console.log(numerazo == "20"); // true
+
+// Comparador estricto ===
+
+console.log(numerazo === "20"); // false
+
+
+// Comparador distinto != 
+
+console.log(numerazo != 33); // true
+
+console.log(numerazo != "20"); // false
+
+console.log(numerazo !== "20"); // true
+
+// Comparador mayor que > 
+
+console.log(numerazo > 50); // false
+
+// Comparador menor que <
+
+console.log(numerazo < 55); // true
+
+// Comparador mayor o igual que > 
+
+console.log(numerazo >= 50); // false
+
+// Comparador menor o igual que > 
+
+console.log(numerazo <= 50); // true
+
 // Logicos
+
+
+let esMayorDeEdad = true;
+let tieneEntrada = true;
+
+console.log(esMayorDeEdad && tieneEntrada); // && Solo es True si ambos enunciados son verdaderos
+
+
+console.log(esMayorDeEdad || tieneEntrada);  // || es True si almenos 1 de sus enunciados son verdaderos
+
+
+// ! Negacion de un expresion == No
+
+console.log(!esMayorDeEdad);  // false
 
 // Cadena
 
-// Incremento y decremento
 
+let mensaje1 = "hola";
+let mensaje2 = "que tal?";
+
+let mensaje_total = mensaje1 + " " + mensaje2;
+
+
+mensaje_total += " Soy Victor Robles WEB";
+
+console.log(mensaje_total);
+
+// Incremento 
+
+let cifra = 1200;
+
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+
+console.log(cifra);
+
+
+// Decremento
+cifra--;
+console.log(cifra);
