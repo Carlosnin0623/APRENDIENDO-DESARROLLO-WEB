abdb149dc10ec255850db452c3bb364f3fab9fa0
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-05-07 10:57:04 -0400

    Ventana emergentes

---
+++ JAVASCRIPT/10-Ventanas-emergentes/main.js
@@ -0,0 +1,13 @@
+// Alertas
+
+let mensaje = "Hola soy Victor Robles Web";
+
+alert(mensaje);
+
+
+// Confirmación
+
+let pregunta = confirm(`Estas seguro de que quieres continuar`);
+
+
+// Promts (Ingreso de datos)
