be15352a42298bd95d138c51e403556624a225a3
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-05-14 17:00:51 -0400

    Scope o alcanse de las variables javascript

--- JAVASCRIPT/12-Funciones/07-Ambito-o-Scope.js
+++ JAVASCRIPT/12-Funciones/07-Ambito-o-Scope.js
@@ -15,5 +15,14 @@ function mostrarRuta(){
     console.log(nombre);
 }
 
-
 mostrarRuta();
+
+if(true){
+
+    let deporte = "Mi deporte favorito es la natación";
+
+    console.log(deporte);
+}
+
+/* console.log(deporte); deporte not is defined porque no podemos accer a las variables 
+de tipo let fuera de su bloque ya que esta no es global*/
---
+++ JAVASCRIPT/12-Funciones/08-Hoisting.js
@@ -0,0 +1,11 @@
+// Hosting o elevacion
+
+var coche = "Ferrari testarrosa";
+
+
+
+console.log(coche);
+
+let moto = "Ducati x1";
+
+console.log(moto);
