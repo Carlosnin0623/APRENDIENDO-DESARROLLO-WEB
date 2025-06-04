dd0840d4102bb2fd4f9020e4248c06d8160f6a01
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-06-04 16:22:11 -0400

    Bom

---
+++ JAVASCRIPT/16-BOM/01-BOM.js
@@ -0,0 +1,13 @@
+/*
+BOM: Browser Object Model, Es un objeto que contiene metodos o propiedades para interactuar con el navegador
+
+Alertas y dialogos
+
+alert("Saludos abuelita este curso lo estan viendo miles de personas");
+
+let nombre = promt("Como se llama tu abuela");
+
+let mayorEdad = confirm("Eres mayor de edad") solo devuelve true o false
+
+
+*/
